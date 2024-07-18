0fb74f62033a37dea93e4d13dbaba4ff29cf9a7e
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-07-19 00:17:58 +0800

    refactor: login ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,61 +1,44 @@
 'use client';
 import React, { useState } from 'react';
-
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 const Login = () => {
-  const [form, setForm] = useState({ fullname: '', email: '', password: '' });
-  const handleSubmit = (e: React.FormEvent<HTMLElement>) => {
+  const [form, setForm] = useState({ fullName: '', email: '', password: '' });
+  const handleRegistrationForm = (e: React.FormEvent<HTMLElement>) => {
     e.preventDefault();
     console.log(form);
   };
   return (
     <div className='border flex justify-center items-center h-screen border-red-500'>
       {/*Left*/}
-      <div className='w-[50%] border h-[70%] border-blue-500 flex justify-center items-center'>
-        Login Text Here
+      <div className='w-[50%] border text-9xl h-[70%] border-blue-500 flex justify-center items-center'>
+        Sign Up
       </div>
       {/*Right*/}
       <div className='w-[50%] border h-[70%] border-green-500 flex justify-center items-center'>
-        <form className='flex flex-col gap-10' onSubmit={handleSubmit}>
-          <div className='flex gap-5'>
-            <label htmlFor='name' className='text-5xl'>
-              FullName
-            </label>
-
-            <input
-              type='text'
-              id='name'
-              className='px-5'
-              onChange={(nameText) => setForm({ ...form, fullname: nameText.target.value })}
-              value={form.fullname}
-            />
-          </div>
-          <div className='flex gap-5'>
-            <label htmlFor='email' className='text-5xl'>
-              Email
-            </label>
-            <input
-              type='text'
-              id='email'
-              className='px-5'
-              onChange={(emailText) => setForm({ ...form, email: emailText.target.value })}
-              value={form.email}
-            />
-          </div>
-          <div className='flex gap-5'>
-            <label htmlFor='password' className='text-5xl'>
-              Password
-            </label>
-            <input
-              type='password'
-              id='password'
-              className='px-5'
-              onChange={(passwordText) => setForm({ ...form, password: passwordText.target.value })}
-              value={form.password}
-            />
-          </div>
-          <button type='submit' className='border rounded-md  py-2 w-[30%]   '>
-            Submit
-          </button>
+        <form className='flex flex-col gap-10 w-[50%]' onSubmit={handleRegistrationForm}>
+          <Input
+            placeholder='Fullname'
+            value={form.fullName}
+            onChange={(fullNameInput) => {
+              setForm({ ...form, fullName: fullNameInput.target.value });
+            }}
+          />
+          <Input
+            placeholder='Email'
+            value={form.email}
+            onChange={(emailInput) => {
+              setForm({ ...form, email: emailInput.target.value });
+            }}
+          />
+          <Input
+            placeholder='Password'
+            value={form.password}
+            onChange={(passwordInput) => {
+              setForm({ ...form, password: passwordInput.target.value });
+            }}
+          />
+          <Button type='submit'>Submit</Button>
         </form>
       </div>
     </div>
