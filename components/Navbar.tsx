7ffad0e49b2fd7721f75094d8a32531b0a014e54
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-07-26 02:30:47 +0800

    refactor: sign up pages cart and navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,8 +13,16 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle
 } from '@/components/ui/navigation-menu';
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalTrigger
+} from '@/components/ui/animatedModal';
 import { Input } from '@/components/ui/input';
 import jwt, { JwtPayload } from 'jsonwebtoken';
+import Cart from './Cart';
 
 const components: { title: string; href: string; description: string }[] = [
   {
@@ -94,69 +102,89 @@ export function Navbar() {
 
   return (
     <div className='border flex justify-between   bg-[#FFD966] px-10 rounded-lg items-center  py-10'>
-      <div>
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem className=''>
-              <NavigationMenuTrigger>Shop</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className='grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
-                  <li className='row-span-3'>
-                    <NavigationMenuLink asChild>
-                      <a
-                        className='flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md'
-                        href='/'
-                      >
-                        <div className='mb-2 mt-4 text-lg font-medium'>shadcn/ui</div>
-                        <p className='text-sm leading-tight text-muted-foreground'>
-                          Beautifully designed components built with Radix UI and Tailwind CSS.
-                        </p>
-                      </a>
-                    </NavigationMenuLink>
-                  </li>
-                  <ListItem href='/docs' title='Introduction'>
-                    Re-usable components built using Radix UI and Tailwind CSS.
-                  </ListItem>
-                  <ListItem href='/docs/installation' title='Installation'>
-                    How to install dependencies and structure your app.
-                  </ListItem>
-                  <ListItem href='/docs/primitives/typography' title='Typography'>
-                    Styles for headings, paragraphs, lists...etc
-                  </ListItem>
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Learn</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] '>
-                  {components.map((component) => (
-                    <ListItem key={component.title} title={component.title} href={component.href}>
-                      {component.description}
+      <Modal>
+        <div>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem className=''>
+                <NavigationMenuTrigger>Shop</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className='grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
+                    <li className='row-span-3'>
+                      <NavigationMenuLink asChild>
+                        <a
+                          className='flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md'
+                          href='/'
+                        >
+                          <div className='mb-2 mt-4 text-lg font-medium'>shadcn/ui</div>
+                          <p className='text-sm leading-tight text-muted-foreground'>
+                            Beautifully designed components built with Radix UI and Tailwind CSS.
+                          </p>
+                        </a>
+                      </NavigationMenuLink>
+                    </li>
+                    <ListItem href='/docs' title='Introduction'>
+                      Re-usable components built using Radix UI and Tailwind CSS.
                     </ListItem>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href='/docs' legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Subscribe
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-      </div>
-      <div className='flex gap-5 items-center'>
-        <div className=''>
-          <Input type='email' placeholder='Find in Store' />
+                    <ListItem href='/docs/installation' title='Installation'>
+                      How to install dependencies and structure your app.
+                    </ListItem>
+                    <ListItem href='/docs/primitives/typography' title='Typography'>
+                      Styles for headings, paragraphs, lists...etc
+                    </ListItem>
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Learn</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] '>
+                    {components.map((component) => (
+                      <ListItem key={component.title} title={component.title} href={component.href}>
+                        {component.description}
+                      </ListItem>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href='/docs' legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Subscribe
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
         </div>
-        <div className='cursor-pointer'>
-          {userName ? (
-            <div>Welcome, {userName}</div>
-          ) : (
-            <Link href={'/sign-up'}>
+        <div className='flex gap-5 items-center'>
+          <div className=''>
+            <Input type='text' placeholder='Find in Store' />
+          </div>
+          <div className='cursor-pointer'>
+            {userName ? (
+              <div>Welcome, {userName}</div>
+            ) : (
+              <Link href={'/sign-up'}>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='size-6'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z'
+                  />
+                </svg>
+              </Link>
+            )}
+          </div>
+          <ModalTrigger>
+            <div className='cursor-pointer'>
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
@@ -168,29 +196,24 @@ export function Navbar() {
                 <path
                   strokeLinecap='round'
                   strokeLinejoin='round'
-                  d='M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z'
+                  d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z'
                 />
               </svg>
-            </Link>
-          )}
+            </div>
+          </ModalTrigger>
+          <ModalBody>
+            <ModalContent>
+              <h4 className='text-lg md:text-2xl text-neutral-600 dark:text-neutral-100 font-bold text-center mb-8'>
+                Check your{' '}
+                <span className='px-1 py-0.5 rounded-md bg-gray-100 dark:bg-neutral-800 dark:border-neutral-700 border border-gray-200'>
+                  Cart
+                </span>{' '}
+              </h4>
+              <Cart />
+            </ModalContent>
+          </ModalBody>
         </div>
-        <div className='cursor-pointer'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            strokeWidth={1.5}
-            stroke='currentColor'
-            className='size-6'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z'
-            />
-          </svg>
-        </div>
-      </div>
+      </Modal>
     </div>
   );
 }
