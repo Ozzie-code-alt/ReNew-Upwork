98a85efff1cf6fee2bb2e00bb9420e7bc224a555
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-07-30 22:53:34 +0800

    refactor  categories page

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -4,75 +4,71 @@ import { motion, useMotionValue } from 'framer-motion';
 
 const StaticPage1 = () => {
   return (
-<section>
-  <div className="mx-auto  px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-    <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:items-stretch">
-      <div className="grid place-content-center rounded bg-gray-100 p-6 sm:p-8">
-        <div className="mx-auto max-w-md text-center lg:text-left">
-          <header>
-            <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">Watches</h2>
+    <section>
+      <div className='mx-auto  px-4 py-8 sm:px-6 sm:py-12 lg:px-8'>
+        <div className='grid grid-cols-1 gap-4 lg:grid-cols-3 lg:items-stretch'>
+          <div className='grid place-content-center rounded bg-gray-100 p-6 sm:p-8'>
+            <div className='mx-auto max-w-md text-center lg:text-left'>
+              <header>
+                <h2 className='text-xl font-bold text-gray-900 sm:text-3xl'>Watches</h2>
 
-            <p className="mt-4 text-gray-500">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas rerum quam amet
-              provident nulla error!
-            </p>
-          </header>
+                <p className='mt-4 text-gray-500'>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas rerum quam amet
+                  provident nulla error!
+                </p>
+              </header>
 
-          <a
-            href="#"
-            className="mt-8 inline-block rounded border border-gray-900 bg-gray-900 px-12 py-3 text-sm font-medium text-white transition hover:shadow focus:outline-none focus:ring"
-          >
-            Shop All
-          </a>
+              <a
+                href='#'
+                className='mt-8 inline-block rounded border border-gray-900 bg-gray-900 px-12 py-3 text-sm font-medium text-white transition hover:shadow focus:outline-none focus:ring'
+              >
+                Shop All
+              </a>
+            </div>
+          </div>
+
+          <div className='lg:col-span-2 lg:py-8'>
+            <ul className='grid grid-cols-2 gap-4'>
+              <li key='1'>
+                <a href='#' className='group block'>
+                  <img
+                    src='https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1598&q=80'
+                    alt=''
+                    className='aspect-square w-full rounded object-cover'
+                  />
+
+                  <div className='mt-3'>
+                    <h3 className='font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4'>
+                      Simple Watch
+                    </h3>
+
+                    <p className='mt-1 text-sm text-gray-700'>$150</p>
+                  </div>
+                </a>
+              </li>
+
+              <li key='2'>
+                <a href='#' className='group block'>
+                  <img
+                    src='https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1598&q=80'
+                    alt=''
+                    className='aspect-square w-full rounded object-cover'
+                  />
+
+                  <div className='mt-3'>
+                    <h3 className='font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4'>
+                      Simple Watch
+                    </h3>
+
+                    <p className='mt-1 text-sm text-gray-700'>$150</p>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-
-      <div className="lg:col-span-2 lg:py-8">
-        <ul className="grid grid-cols-2 gap-4">
-          <li>
-            <a href="#" className="group block">
-              <img
-                src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1598&q=80"
-                alt=""
-                className="aspect-square w-full rounded object-cover"
-              />
-
-              <div className="mt-3">
-                <h3
-                  className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4"
-                >
-                  Simple Watch
-                </h3>
-
-                <p className="mt-1 text-sm text-gray-700">$150</p>
-              </div>
-            </a>
-          </li>
-
-          <li>
-            <a href="#" className="group block">
-              <img
-                src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1598&q=80"
-                alt=""
-                className="aspect-square w-full rounded object-cover"
-              />
-
-              <div className="mt-3">
-                <h3
-                  className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4"
-                >
-                  Simple Watch
-                </h3>
-
-                <p className="mt-1 text-sm text-gray-700">$150</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</section>
+    </section>
   );
 };
 
@@ -81,8 +77,8 @@ const StaticPage2 = () => {
     <section>
       <div className='mx-auto  px-4 py-8 sm:px-6 lg:px-8'>
         <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
-          <div className='bg-blue-600 p-8 md:p-12 lg:px-16 lg:py-24'>
-            <div className='mx-auto max-w-xl text-center'>
+          <div className='bg-blue-600 p-8  flex flex-col justify-center items-center md:p-12 lg:px-16 lg:py-24'>
+            <div className='mx-auto flex flex-col justify-center items-center max-w-xl text-left'>
               <h2 className='text-2xl font-bold text-white md:text-3xl'>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit
               </h2>
@@ -125,8 +121,8 @@ const StaticPage2 = () => {
 
 const StaticPage3 = () => {
   return (
-    <section className='overflow-hidden bg-gray-50 sm:grid sm:grid-cols-2'>
-      <div className='p-8 md:p-12 lg:px-16 '>
+    <section className='overflow-hidden h-[80%] my-20 bg-gray-50 sm:grid sm:grid-cols-2'>
+      <div className='p-8 md:p-12 flex flex-col justify-center items-center lg:px-16 '>
         <div className='mx-auto  text-center ltr:sm:text-left rtl:sm:text-right'>
           <h2 className='text-2xl font-bold text-gray-900 md:text-3xl'>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit
@@ -158,7 +154,11 @@ const StaticPage3 = () => {
   );
 };
 
-const pages = [<StaticPage1 />, <StaticPage2 />, <StaticPage3 />];
+const pages = [
+  <StaticPage1 key='page1' />,
+  <StaticPage2 key='page2' />,
+  <StaticPage3 key='page3' />
+];
 
 const ONE_SECOND = 1000;
 const AUTO_DELAY = ONE_SECOND * 10;
@@ -221,8 +221,8 @@ export const SwipeCarousel = () => {
         onDragEnd={onDragEnd}
         className='flex cursor-grab items-center active:cursor-grabbing'
       >
-        {pages.map((Page, idx) => (
-          <div key={idx} className='h-screen w-screen shrink-0'>
+        {pages.map((Page, index) => (
+          <div key={index} className='h-screen w-screen shrink-0'>
             {Page}
           </div>
         ))}
